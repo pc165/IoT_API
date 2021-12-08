8a21b52b22a05c4f79007d2cb998e120dff7e613
p014 <pc165@pm.me>
2021-12-08 21:43:41 +0100

    update start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 source ./env/bin/activate 
-python -m pip install fastapi "uvicorn[standard]" aiosqlite sqlalchemy "python-jose[cryptography]" "passlib[bcrypt]" python-multipart
+python -m pip install -r requirements.txt
 python -m uvicorn main:app --reload

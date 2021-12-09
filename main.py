8a5e7ec1d67c7908432dab1a5938cca65a8972b6
p014 <pc165@pm.me>
2021-12-09 23:13:29 +0100

    .

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ app.include_router(item_router)
 app.include_router(login_router)
 
 
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+# @app.on_event("startup")
+# async def startup():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
